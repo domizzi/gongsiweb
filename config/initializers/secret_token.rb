44a5df41e38c52c34aa78a3d69072cc3968d34c0
dengenming@outlook.com <dengenming@outlook.com>
2013-04-24 16:28:47 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gongsiweb::Application.config.secret_token = 'a847ff34c886a5725cd74ab376786dba3b3ee2983321505109199efdb120afcc8df9a8497f1ff3ffe7523891b17936ab1a55d34487d990b2b1bf95f47d7760cb'
